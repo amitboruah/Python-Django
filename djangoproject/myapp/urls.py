0edebf49a95624498e538be16c4d3221c3619be9
Amit Boruah <66059196+amitboruah@users.noreply.github.com>
2021-06-08 12:00:01 +0530

    Delete djangoproject directory

--- djangoproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from.import views
-
-urlpatterns = [
-    path('',views.homepageview,name='home'),
-    path('about',views.aboutpageview,name='about'),
-    path('contact',views.contactpageview,name='contact'),
-]
